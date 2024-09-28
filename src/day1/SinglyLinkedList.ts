88b66f84bb93c087e97a4473b01d0b43ee3ca12c
i-kamran <ibragimovkmrn@gmail.com>
2024-09-28 21:41:03 +0500

    feat: Add ListNode type

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,21 @@
 export default class SinglyLinkedList<T> {
     public length: number;
-
-    
-
+    private head?: ListNode<T>;
+    private tail?: ListNode<T>
     constructor() {
+        this.head = this.tail = undefined
+        this.lenght = 0
     }
 
-    prepend(item: T): void {
-
+    prepend(item: T): void { }
+    insertAt(item: T, idx: number): void { }
+    append(item: T): void { }
+    remove(item: T): T | undefined { }
+    get(idx: number): T | undefined { }
+    removeAt(idx: number): T | undefined { }
 }
-    insertAt(item: T, idx: number): void {
 
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+type ListNode<T> = {
+    value: <T>,
+    next?: ListNode<T>
 }
