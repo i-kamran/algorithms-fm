f3285aee5cd8e37075867e780d292fabdcdf4640
i-kamran <ibragimovkmrn@gmail.com>
2024-10-06 23:42:34 +0500

    feat: Add implementation for TwoCrystalBalls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -3,4 +3,14 @@ export default function two_crystal_balls(breaks: boolean[]): number {
     const jump = Math.floor(Math.sqrt(arrLen));
 
     let i = jump;
+    for (; i < arrLen; i += jump) {
+        if (breaks[i]) {
+            i -= jump;
+            for (let stop = i * jump; i < stop; i++) {
+                if (breaks[i]) {
+                    return i;
+                }
+            }
+        }
+    }
 }
