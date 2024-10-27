12a981b482aeefffb94cf547c157a000142fb58d
i-kamran <ibragimovkmrn@gmail.com>
2024-10-27 23:21:12 +0500

    feat: Implement pop and peek methods

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -2,23 +2,35 @@ export default class Stack<T> {
     public length: number;
     private pointer?: StackNode<T>;
 
-    constructor() {
+    constructor() { 
         this.length = 0;
         this.pointer = undefined;
     }
 
-    push(item: T): void {
+    push(item: T): void { 
         const node: StackNode<T> = { value: item, next: this.pointer };
         this.pointer = node;
         this.length++;
     }
 
-    pop(): T | undefined { }
+    pop(): T | undefined { 
+        if (this.length === 0) {
+            return undefined;
+        }
+        
+        const node = this.pointer;
+        this.pointer = this.pointer?.next;
+        this.length--;
+        return node?.value;
+    }
 
-    peek(): T | undefined { }
+    peek(): T | undefined { 
+        return this.pointer?.value;
+    }
 }
 
 type StackNode<T> = {
     value: T;
     next?: StackNode<T>;
 };
+
