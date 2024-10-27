fa47f70ec00866a603272b36495d261b64af081e
i-kamran <ibragimovkmrn@gmail.com>
2024-10-27 23:19:55 +0500

    feat: Implement StackNode type and push method

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,24 @@
 export default class Stack<T> {
     public length: number;
-
-    
+    private pointer?: StackNode<T>;
 
     constructor() {
+        this.length = 0;
+        this.pointer = undefined;
     }
 
     push(item: T): void {
+        const node: StackNode<T> = { value: item, next: this.pointer };
+        this.pointer = node;
+        this.length++;
+    }
 
-}
-    pop(): T | undefined {
+    pop(): T | undefined { }
 
+    peek(): T | undefined { }
 }
-    peek(): T | undefined {
 
-}
-}
+type StackNode<T> = {
+    value: T;
+    next?: StackNode<T>;
+};
