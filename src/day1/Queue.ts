b39a012da91473fc7df327f7c34db6add1c66a32
i-kamran <ibragimovkmrn@gmail.com>
2024-11-08 22:42:01 +0500

    feat: Add enque method and QueueNode type

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,27 @@
 export default class Queue<T> {
     public length: number;
-
-    
-
+    private head?: QueueNode<T>;
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const newNode: QeueuNode<T> = { value: item, next: undefined };
+        if (this.head == undefined) {
+            this.head = newNode;
+        }
+        else {
+            newNode.prev = this.head
+            this.head = newNode
+        }
+        this.length++;
+    }
+    deque(): T | undefined {}
+    peek(): T | undefined {}
+}
+type QueueNode<T> = {
+    value: T;
+    prev?: QueueNode<T>;
+};
 
-}
-    deque(): T | undefined {
-
-}
-    peek(): T | undefined {
-
-}
-}
