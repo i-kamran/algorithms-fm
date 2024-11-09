1812097d7e11685f09f906e61e90a99ec315442d
i-kamran <ibragimovkmrn@gmail.com>
2024-11-09 22:47:42 +0500

    feat: Change enqueue and add deque methods

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,27 +1,51 @@
 export default class Queue<T> {
     public length: number;
     private head?: QueueNode<T>;
+    private tail?: QueueNode<T>;
+
     constructor() {
-        this.head = this.tail = undefined;
+        this.head = undefined;
+        this.tail = undefined;
         this.length = 0;
     }
 
     enqueue(item: T): void {
-        const newNode: QeueuNode<T> = { value: item, next: undefined };
-        if (this.head == undefined) {
-            this.head = newNode;
-        }
-        else {
-            newNode.prev = this.head
-            this.head = newNode
+        const newNode: QueueNode<T> = { value: item, next: undefined };
+        
+        if (this.tail === undefined) {
+            // If queue is empty, set both head and tail to new node
+            this.head = this.tail = newNode;
+        } else {
+            // Add to tail
+            this.tail.next = newNode;
+            this.tail = newNode;
         }
         this.length++;
     }
-    deque(): T | undefined {}
-    peek(): T | undefined {}
+
+    deque(): T | undefined {
+        if (this.head === undefined) {
+            return undefined;
+        }
+
+        const value = this.head.value;
+        this.head = this.head.next;
+        this.length--;
+
+        if (this.length === 0) {
+            // If queue is now empty, reset tail
+            this.tail = undefined;
+        }
+
+        return value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
 }
+
 type QueueNode<T> = {
     value: T;
-    prev?: QueueNode<T>;
+    next?: QueueNode<T>;
 };
-
