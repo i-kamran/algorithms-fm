40654855b91b08fff803f114456cdab16d861b4f
i-kamran <ibragimovkmrn@gmail.com>
2024-11-10 23:07:16 +0500

    feat: Add method in DoublyLinkedList

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -8,15 +8,14 @@ export default class DoublyLinkedList<T> {
     public length: number;
     private head?: ListNode<T>;
     private tail?: ListNode<T>;
-    
 
     constructor() {
-        this.length = 0
-        this.head = this.tail = undefined
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     prepend(item: T): void {
-         this.length++;
+        this.length++;
         const node = { value: item } as ListNode<T>;
         if (!this.head) {
             this.head = this.tail = node;
@@ -25,21 +24,24 @@ export default class DoublyLinkedList<T> {
         this.head.prev = node;
         node.next = this.head;
         this.head = node;
-
-}
-    insertAt(item: T, idx: number): void {
-
-}
+    }
+    insertAt(item: T, idx: number): void {}
     append(item: T): void {
+        const node = { value: item } as ListNode<T>;
+        this.length++;
 
-}
-    remove(item: T): T | undefined {
+        // If the list is empty, set head and tail to the new node
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
 
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+        // Attach the new node to the end of the list
+        node.prev = this.tail;
+        this.tail.next = node;
+        this.tail = node;
+    }
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}
 }
