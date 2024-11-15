2784828ba6df8774a5f9ee416a2c5cf317ccdfe1
i-kamran <ibragimovkmrn@gmail.com>
2024-11-15 23:43:06 +0500

    feat: Implement quick sort

--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,3 +1,42 @@
 export default function quick_sort(arr: number[]): void {
-
+ if (arr.length <= 1) return;
+  
+  function partition(left: number, right: number): number {
+    const pivot = arr[Math.floor((left + right) / 2)];
+    let i = left;
+    let j = right;
+    
+    while (i <= j) {
+      while (arr[i] < pivot) {
+        i++;
+      }
+      
+      while (arr[j] > pivot) {
+        j--;
+      }
+      
+      if (i <= j) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+        i++;
+        j--;
+      }
+    }
+    
+    return i;
+  }
+  
+  function sort(left: number, right: number): void {
+    let index = partition(left, right);
+    
+    if (left < index - 1) {
+      sort(left, index - 1);
+    }
+    
+    if (index < right) {
+      sort(index, right);
+    }
+  }
+  
+  sort(0, arr.length - 1);
+}
 }
