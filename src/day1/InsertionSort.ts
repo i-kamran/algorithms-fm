f01ec2c5b230de7175607bd046fdc50f8e9cfe39
i-kamran <ibragimovkmrn@gmail.com>
2024-11-17 21:45:02 +0500

    chore: Use const for immutable variables

--- src/day1/InsertionSort.ts
+++ src/day1/InsertionSort.ts
@@ -1,6 +1,6 @@
 export default function insertion_sort(arr: number[]): void {
     for (let i = 1; i < arr.length; ++i) {
-        let current = arr[i];
+        const current = arr[i];
         let j = i - 1;
 
         while (j >= 0 && arr[j] > current) {
--- src/day1/QuickSort.ts
+++ src/day1/QuickSort.ts
@@ -1,42 +1,41 @@
 export default function quick_sort(arr: number[]): void {
- if (arr.length <= 1) return;
-  
-  function partition(left: number, right: number): number {
-    const pivot = arr[Math.floor((left + right) / 2)];
-    let i = left;
-    let j = right;
-    
-    while (i <= j) {
-      while (arr[i] < pivot) {
-        i++;
-      }
-      
-      while (arr[j] > pivot) {
-        j--;
-      }
-      
-      if (i <= j) {
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-        i++;
-        j--;
-      }
+    if (arr.length <= 1) return;
+
+    function partition(left: number, right: number): number {
+        const pivot = arr[Math.floor((left + right) / 2)];
+        let i = left;
+        let j = right;
+
+        while (i <= j) {
+            while (arr[i] < pivot) {
+                i++;
+            }
+
+            while (arr[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+                i++;
+                j--;
+            }
+        }
+
+        return i;
     }
-    
-    return i;
-  }
-  
-  function sort(left: number, right: number): void {
-    let index = partition(left, right);
-    
-    if (left < index - 1) {
-      sort(left, index - 1);
+
+    function sort(left: number, right: number): void {
+        const index = partition(left, right);
+
+        if (left < index - 1) {
+            sort(left, index - 1);
+        }
+
+        if (index < right) {
+            sort(index, right);
+        }
     }
-    
-    if (index < right) {
-      sort(index, right);
-    }
-  }
-  
-  sort(0, arr.length - 1);
-}
+
+    sort(0, arr.length - 1);
 }
