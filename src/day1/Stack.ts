a4b38b027a90b997fd1a889cbb3338173a64c498
i-kamran <ibragimovkmrn@gmail.com>
2024-11-29 19:22:40 +0500

    feat: Add new stack implementation

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,36 +1,39 @@
-export default class Stack<T> {
-    public length: number;
-    private pointer?: StackNode<T>;
-
-    constructor() { 
-        this.length = 0;
-        this.pointer = undefined;
-    }
-
-    push(item: T): void { 
-        const node: StackNode<T> = { value: item, next: this.pointer };
-        this.pointer = node;
-        this.length++;
-    }
-
-    pop(): T | undefined { 
-        if (this.length === 0) {
-            return undefined;
-        }
-        
-        const node = this.pointer;
-        this.pointer = this.pointer?.next;
-        this.length--;
-        return node?.value;
-    }
-
-    peek(): T | undefined { 
-        return this.pointer?.value;
-    }
-}
-
 type StackNode<T> = {
     value: T;
     next?: StackNode<T>;
 };
+export default class Stack<T> {
+    private head?: StackNode<T>;
+    length: number;
+    constructor() {
+        this.length = 0;
+        this.head = undefined;
+    }
 
+    push(item: T): void {
+        this.length++;
+        const newNode: StackNode<T> = { value: item };
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    pop(): T | undefined {
+        if (!this.head) return undefined;
+        this.length--;
+        const value = this.head.value;
+        if (!this.head.next) {
+            this.head = undefined;
+            return value;
+        }
+        this.head = this.head.next;
+        return value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
+}
