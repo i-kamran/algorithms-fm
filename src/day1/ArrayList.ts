9c8217a4d4e0e909375e6611f1a34265be1b5af6
i-kamran <ibragimovkmrn@gmail.com>
2024-11-30 23:08:40 +0500

    feat: Add get and change prepend methods in ArrayList

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -18,15 +18,37 @@ export default class ArrayList<T> {
     }
 
     prepend(item: T): void {
+        // add and then grow
+        if (this.capacity === this.length) {
+           const newCapacity = this.capacity * 2;
+           const newArr = new Array(newCapacity)
+            for (let i = 1; i <= this.length; ++i ){
+                    newArr[i] = this.arr[i-1]
+            }
+            newArr[0] = item
+            this.arr = newArr
+            this.length++
+            return
+        }
+        for (let i = this.length; i > 0; --i) {
+            this.arr[i] = this.arr[i - 1];
+        }
+        this.arr[0] = item
+        this.length++;
+    }
+    insertAt(item: T, idx: number): void {}
+    append(item: T): void {
         if (this.capacity === this.length) {
             this.growArray(this.capacity * 2);
         }
-        
+        this.arr[this.length] = item;
+        this.length++;
     }
-    insertAt(item: T, idx: number): void {}
-    append(item: T): void {}
     remove(item: T): T | undefined {}
-    get(idx: number): T | undefined {}
+    get(idx: number): T | undefined {
+        if (idx >= this.length) return undefined
+        return this.arr[idx]
+    }
     removeAt(idx: number): T | undefined {}
 }
 
