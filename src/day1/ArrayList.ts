e7ed03794f3d3fc5274f23345fb803917e83e7d8
i-kamran <ibragimovkmrn@gmail.com>
2024-11-30 20:59:16 +0500

    feat: Add growArray method in ArrayList

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,32 @@
 export default class ArrayList<T> {
     public length: number;
+    public capacity: number;
+    private arr: T[];
 
-    
-
-    constructor() {
+    constructor(initialCapacity: number = 10) {
+        this.length = 0;
+        this.capacity = initialCapacity;
+        this.arr = new Array(initialCapacity);
+    }
+    private growArray(newCapacity: number): void {
+        const newArr = new Array(newCapacity);
+        for (let i = 0; i < this.capacity; ++i) {
+            newArr[i] = this.arr[i];
+        }
+        this.arr = newArr;
+        this.capacity = newCapacity;
     }
 
     prepend(item: T): void {
+        if (this.capacity === this.length) {
+            this.growArray(this.capacity * 2);
+        }
+        
+    }
+    insertAt(item: T, idx: number): void {}
+    append(item: T): void {}
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}
+}
 
-}
-    insertAt(item: T, idx: number): void {
-
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
-}
