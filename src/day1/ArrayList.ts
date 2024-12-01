6bd7b8907e226aae99018c672eebccdf6ce1109c
i-kamran <ibragimovkmrn@gmail.com>
2024-12-01 21:45:15 +0500

    feat: Add insertAt method in ArrayList

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -3,7 +3,7 @@ export default class ArrayList<T> {
     public capacity: number;
     private arr: T[];
 
-    constructor(initialCapacity: number = 10) {
+    constructor(initialCapacity: number = 2) {
         this.length = 0;
         this.capacity = initialCapacity;
         this.arr = new Array(initialCapacity);
@@ -20,23 +20,37 @@ export default class ArrayList<T> {
     prepend(item: T): void {
         // add and then grow
         if (this.capacity === this.length) {
-           const newCapacity = this.capacity * 2;
-           const newArr = new Array(newCapacity)
-            for (let i = 1; i <= this.length; ++i ){
-                    newArr[i] = this.arr[i-1]
+            const newCapacity = this.capacity * 2;
+            const newArr = new Array(newCapacity);
+            for (let i = 1; i <= this.length; ++i) {
+                newArr[i] = this.arr[i - 1];
             }
-            newArr[0] = item
-            this.arr = newArr
-            this.length++
-            return
+            newArr[0] = item;
+            this.arr = newArr;
+            this.length++;
+            return;
         }
         for (let i = this.length; i > 0; --i) {
             this.arr[i] = this.arr[i - 1];
         }
-        this.arr[0] = item
+        this.arr[0] = item;
         this.length++;
     }
-    insertAt(item: T, idx: number): void {}
+    insertAt(item: T, idx: number): void {
+        if (idx < 0 || idx > this.length) {
+            throw new Error(
+                `Index ${idx} is out of bounds. Valid range is 0 to ${this.length}.`,
+            );
+        }
+        if (this.length === this.capacity) {
+            this.growArray(this.capacity * 2);
+        }
+        for (let i = this.length; i > idx; --i) {
+            this.arr[i - 1] = this.arr[i];
+        }
+        this.arr[idx] = item;
+        this.length++;
+    }
     append(item: T): void {
         if (this.capacity === this.length) {
             this.growArray(this.capacity * 2);
