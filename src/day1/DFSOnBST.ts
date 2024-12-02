7bbd621e59fc7463a81588b687f28c3a44986c7d
i-kamran <ibragimovkmrn@gmail.com>
2024-12-02 23:16:12 +0500

    feat: Implement DFS on Binary Search Tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,10 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-
+    return walk(head, needle);
 }
+function walk(curr: BinaryNode<number> | null, needle: number): boolean {
+    if (!curr) return false;
+    if (curr.value === needle) return true;
+
+    return walk(curr.left, needle) || walk(curr.right, needle);
+}
+
