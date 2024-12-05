f2ca9c63c20661cc416a5c7f0309afd9365cce20
i-kamran <ibragimovkmrn@gmail.com>
2024-12-05 23:36:43 +0500

    feat: Add method to get parent idx in heap

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,15 @@
 export default class MinHeap {
     public length: number;
-
-    
+    private capacity: number;
+    private data: number[];
 
     constructor() {
+        this.data = [];
+        this.length = 0;
     }
-
-    insert(value: number): void {
-
-}
-    delete(): number {
-
-}
+    insert(value: number): void {}
+    delete(): number {}
+    private getParent(idx: number): number {
+        return Math.floor((idx - 1) / 2);
+    }
 }
