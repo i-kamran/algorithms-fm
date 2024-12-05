841354345b94c6bfb94ffca41c16638733b6fe73
i-kamran <ibragimovkmrn@gmail.com>
2024-12-05 23:51:24 +0500

    feat: Add insert and delete in MinHeap

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -6,8 +6,23 @@ export default class MinHeap {
         this.data = [];
         this.length = 0;
     }
-    insert(value: number): void {}
-    delete(): number {}
+    insert(value: number): void {
+        this.data[this.length] = value;
+        this.heapifyUp(this.length);
+        this.length++;
+    }
+    delete(): number {
+        if (this.length === 0) return -1;
+        const value = this.data[0];
+        this.length--;
+        if (this.length === 0) {
+            this.data = [];
+            return value;
+        }
+        this.data[0] = this.data[this.length];
+        this.heapifyDown(0);
+        return value;
+    }
     private getParent(idx: number): number {
         return Math.floor((idx - 1) / 2);
     }
@@ -20,7 +35,7 @@ export default class MinHeap {
     private heapifyUp(idx: number): void {
         if (idx === 0) return;
         const parentIdx = this.getParent(idx);
-        if (this.data[idx] > this.data[parentIdx]) {
+        if (this.data[idx] < this.data[parentIdx]) {
             this.swap(this.data, idx, parentIdx);
             this.heapifyUp(parentIdx);
         }
@@ -30,12 +45,14 @@ export default class MinHeap {
         const rightIdx = this.getRightChild(idx);
         if (idx >= this.length || leftIdx >= this.length) return;
 
-        const minChild = Math.min(this.data[leftIdx], this.data[rightIdx]);
-        if (this.data[idx] > minChild) {
-            if (this.data[idx] > this.data[leftIdx]) {
-                this.swap(this.data, idx, leftIdx);
-                this.heapifyDown(leftIdx);
-            }
+        const left = this.data[leftIdx];
+        const right = this.data[rightIdx];
+        const value = this.data[idx];
+
+        if (left < right && left < value) {
+            this.swap(this.data, idx, leftIdx);
+            this.heapifyDown(leftIdx);
+        } else if (right < left && right < value) {
             this.swap(this.data, idx, rightIdx);
             this.heapifyDown(rightIdx);
         }
