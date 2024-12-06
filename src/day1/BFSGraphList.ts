d207fceeeee10dc4b0c3035c7913c8a60b96e1ef
i-kamran <ibragimovkmrn@gmail.com>
2024-12-06 23:09:20 +0500

    feat: Implement BFS on graph list

--- src/day1/BFSGraphList.ts
+++ src/day1/BFSGraphList.ts
@@ -1,7 +1,36 @@
-function walk(graph:WeightedAdjacencyList, curr: number, needle: number, seen: boolen[], path: number[]): boolean{
-    if (curr === needle) return true
+function walk(
+    graph: WeightedAdjacencyList,
+    curr: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    if (seen[curr]) return false;
+    seen[curr] = true;
+    path.push(curr);
+    if (curr === needle) {
+        return true;
+    }
+    const list = graph[curr];
+    for (let i = 0; i < list.length; ++i) {
+        const edge = list[i];
+        if (walk(graph, edge.to, needle, seen, path)) {
+            return true;
+        }
+    }
+    path.pop();
+    return false
 }
-export default function bfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
-
+export default function bfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false)
+    const path: number[] = []
+    walk(graph, source, needle, seen, path)
+    if (path.length ===0 ){
+        return null
+    }
+    return path
 }
-
