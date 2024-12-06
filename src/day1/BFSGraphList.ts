4092cb6fbfefbf668c5e1b180205c98418afba0a
i-kamran <ibragimovkmrn@gmail.com>
2024-12-06 22:50:27 +0500

    feat: Add walk helper function in WeightedAdjList

--- src/day1/BFSGraphList.ts
+++ src/day1/BFSGraphList.ts
@@ -1,3 +1,6 @@
+function walk(graph:WeightedAdjacencyList, curr: number, needle: number, seen: boolen[], path: number[]): boolean{
+    if (curr === needle) return true
+}
 export default function bfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
 
 }
