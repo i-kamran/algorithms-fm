ce1a477f6ee2d67be74398c2055020db42a79ac2
i-kamran <ibragimovkmrn@gmail.com>
2024-12-07 21:43:42 +0500

    feat: Add getLowestUnvisited in DijkstraList

--- src/day1/DijkstraList.ts
+++ src/day1/DijkstraList.ts
@@ -1,13 +1,29 @@
-function hasUnvisited(seen: boolean[], dists: number[]): boolean{
-    return seen.some((s, i) => !s && dists[i] < Infinity)
+function hasUnvisited(seen: boolean[], dists: number[]): boolean {
+    return seen.some((s, i) => !s && dists[i] < Infinity);
 }
-export default function dijkstra_list(source: number, sink: number, arr: WeightedAdjacencyList): number[] {
+function getLowestUnvisited(seen: boolean[], dists: number[]): number {
+    let idx = -1;
+    let lowestDistance = Infinity;
+    for (let i = 0; i < seen.length; ++i) {
+        if (seen[i]) continue;
+        if (lowestDistance > dists[i]) {
+            lowestDistance = dists[i];
+            idx = i;
+        }
+    }
+    return idx
+}
+export default function dijkstra_list(
+    source: number,
+    sink: number,
+    arr: WeightedAdjacencyList,
+): number[] {
+    const seen = new Array(arr.length).fill(false);
+    const dists = new Array(arr.length).fill(Infinity);
+    dists[source] = 0;
 
-    const seen = new Array(arr.length).fill(false)
-    const dists = new Array(arr.length).fill(Infinity)
-    dists[source] = 0
-
-while(hasUnvisited(seen, dists)){
-        const curr = getLowestUnvisited(seen, dists)
+    while (hasUnvisited(seen, dists)) {
+        const curr = getLowestUnvisited(seen, dists);
     }
 }
+
