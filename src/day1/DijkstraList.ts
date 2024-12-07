081b455f3d408db02c09719f01bef1ebc3b98698
i-kamran <ibragimovkmrn@gmail.com>
2024-12-07 21:33:15 +0500

    feat: Initial structure for Dijkstra's algorithm

--- src/day1/DijkstraList.ts
+++ src/day1/DijkstraList.ts
@@ -1,3 +1,10 @@
 export default function dijkstra_list(source: number, sink: number, arr: WeightedAdjacencyList): number[] {
 
+    const seen = new Array(arr.length).fill(false)
+    const dists = new Array(arr.length).fill(Infinity)
+    dists[source] = 0
+
+while(hasUnvisited(seen, dists)){
+        const curr = getLowestUnvisited(seen, dists)
+    }
 }
