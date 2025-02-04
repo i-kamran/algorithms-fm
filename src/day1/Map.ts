8526194752d6faed06e58dd482e412a13e574590
i-kamran <ibragimovkmrn@gmail.com>
2025-02-04 18:35:50 +0500

    feat(Map): Add private class Entry

--- src/day1/Map.ts
+++ src/day1/Map.ts
@@ -1,21 +1,22 @@
-export default class Map<T extends (string | number), V> {
-    
-
-    
-
-    constructor() {
-    }
+export default class Map<T extends string | number, V> {
 
     get(key: T): V | undefined {
+    }
 
-}
     set(key: T, value: V): void {
+    }
+    delete(key: T): V | undefined {}
 
-}
-    delete(key: T): V | undefined {
-
-}
     size(): number {
+        return length;
+    }
 
 }
+class Entry<T extends string | number, V> {
+    private key: T;
+    private value: V;
+    constructor(key: T, value: V) {
+        this.key = key;
+        this.value = value;
+    }
 }
